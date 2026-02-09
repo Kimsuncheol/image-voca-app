52a9d0ab73482b590b9405275e3a98e0864181f5
Benjamin Kim <curetail144@gmail.com>
2026-02-09 17:21:47 +0900

    refactor: Adjust swipeContainer height in vocabulary component for improved layout consistency

--- app/course/[courseId]/vocabulary.tsx
+++ app/course/[courseId]/vocabulary.tsx
@@ -404,7 +404,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   swipeContainer: {
-    height: height * 0.8,
+    height: height * 0.76,
     width: width,
     alignItems: "center",
     justifyContent: "center",
