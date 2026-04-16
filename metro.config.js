6cf2d19467369d199e14e018d7b64d04c32b50fc
Benjamin Kim <curetail144@gmail.com>
2026-04-16 23:38:09 +0900

    chore: update app configuration by removing expo-video and adding react-native-google-mobile-ads with respective app IDs

---
+++ metro.config.js
@@ -0,0 +1,21 @@
+const { getDefaultConfig } = require("expo/metro-config");
+
+const config = getDefaultConfig(__dirname);
+const defaultResolveRequest = config.resolver.resolveRequest;
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (
+    (platform === "android" || platform === "ios") &&
+    moduleName === "firebase/auth"
+  ) {
+    return context.resolveRequest(context, "@firebase/auth", platform);
+  }
+
+  if (defaultResolveRequest) {
+    return defaultResolveRequest(context, moduleName, platform);
+  }
+
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+module.exports = config;
---
+++ src/types/firebase-auth.d.ts
@@ -0,0 +1,8 @@
+import type ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import type { Persistence } from "firebase/auth";
+
+declare module "firebase/auth" {
+  export function getReactNativePersistence(
+    storage: typeof ReactNativeAsyncStorage,
+  ): Persistence;
+}
