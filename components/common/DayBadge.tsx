04ea601e4df470149514624834ab45ae9e6c01d1
Benjamin Kim <curetail144@gmail.com>
2026-04-30 00:12:24 +0900

    refactor: enhance DayBadge component by integrating theme-based styling and improving color management with getFontColors function

--- components/common/DayBadge.tsx
+++ components/common/DayBadge.tsx
@@ -1,6 +1,7 @@
-import { FontColors } from "@/constants/fontColors";
+import { getFontColors } from "@/constants/fontColors";
 import { FontSizes } from "@/constants/fontSizes";
 import { FontWeights } from "@/constants/fontWeights";
+import { useTheme } from "@/src/context/ThemeContext";
 import React from "react";
 import { StyleSheet } from "react-native";
 import { ThemedText } from "../themed-text";
@@ -10,21 +11,28 @@ interface DayBadgeProps {
 }
 
 export function DayBadge({ day }: DayBadgeProps) {
+  const { isDark } = useTheme();
+  const styles = getStyles(isDark);
+  
   return <ThemedText style={styles.badge}>Day {day}</ThemedText>;
 }
 
-const styles = StyleSheet.create({
-  badge: {
-    fontSize: FontSizes.label,
-    color: FontColors.dark.dayBadge,
-    fontWeight: FontWeights.semiBold,
-    opacity: 0.6,
-    paddingHorizontal: 8,
-    paddingVertical: 2,
-    borderRadius: 8,
-    overflow: "hidden",
-    backgroundColor: "transparent",
-    alignSelf: "flex-start",
-    flexShrink: 0,
-  },
-});
+const getStyles = (isDark: boolean) => {
+  const fontColors = getFontColors(isDark);
+
+  return  StyleSheet.create({
+    badge: {
+      fontSize: FontSizes.label,
+      color: fontColors.dayBadge,
+      fontWeight: FontWeights.semiBold,
+      opacity: 0.6,
+      paddingHorizontal: 8,
+      paddingVertical: 2,
+      borderRadius: 8,
+      overflow: "hidden",
+      backgroundColor: "transparent",
+      alignSelf: "flex-start",
+      flexShrink: 0,
+    },
+  });
+};
